2934b16969d28364069ee51b5677e1bf61d5dcea
EthanSuperior <evan.k.chase@gmail.com>
2023-09-25 23:42:28 -0600

    Revert "Merge branch 'Basic-Networking' of https://github.com/EthanSuperior/ThreeSquareSquires into Basic-Networking"
    This reverts commit 02e3f20fd9c8fbd5c9a2854330dd6a41e66bd32c, reversing
    changes made to 725136cbb81789fd01df341f256739636627c9e2.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,15 @@
 extends CharacterBody2D
 
 enum{
-	up = -1,
-	down = 1,
-	left = -1,
-	right = 1,
-	none = 0
+	up,
+	down,
+	left,
+	right,
+	none
 }
 
 #Initialize for movement
 var tileSize = 4
-var boardPosition = Vector2(0,0)
 
 #Setting stage of turn
 var stage = 0
@@ -35,12 +34,12 @@ func turnFlow():
 #//////////////Functions relating to stage 1: movement
 var spacesLeft = 4
 var storedMovement = []
-var selectorPos = boardPosition
 func movement():
 	if spacesLeft>0:
-		selectorPos += squareMovement()
-		if Input.is_action_just_pressed('select'):
-			squareSelection()
+		var direction = checkInputDirection()
+		squareSelection(direction)
+		if direction != none:
+			temp(direction)
 		if Input.is_action_just_pressed("return") and !storedMovement.is_empty():
 			goBack()
 	else:
@@ -49,36 +48,49 @@ func movement():
 
 
 #Controls moving selection square
-func squareSelection():
-	var change = selectorPos-boardPosition
-	if change.length() == 1:
-		global_position += change*tileSize
-		boardPosition = selectorPos
-		$squareSelector.global_position = global_position
-		selectorPos = boardPosition
-		storedMovement.append(change)
-		spacesLeft -= 1
-		print(spacesLeft)
+func squareSelection(direction):
+	match direction:
+		right:
+			global_position.x += tileSize
+		left:
+			global_position.x -= tileSize
+		up:
+			global_position.y -= tileSize
+		down:
+			global_position.y += tileSize
+		none:
+			return
 
+func temp(direction):
+	storedMovement.append(direction)
+	spacesLeft -=1
 
 func goBack():
-	global_position += -storedMovement.pop_back()*tileSize
-	spacesLeft +=1
+	var direction = none
+	match storedMovement.pop_back():
+		up:
+			direction = down
+		left:
+			direction = right
+		right:
+			direction = left
+		down:
+			direction = up
+	squareSelection(direction)
+	spacesLeft+=1
+
+func checkInputDirection():
+	var direction = none
+	if Input.is_action_just_pressed('move_right'):
+		direction = right
+	if Input.is_action_just_pressed("move_left"):
+		direction = left
+	if Input.is_action_just_pressed("move_up"):
+		direction = up
+	if Input.is_action_just_pressed("move_down"):
+		direction = down
+	return direction
 
 #//////////////Function for stage 2:Trap Selection
 func traps():
 	return
-
-#Function for moving selection Square
-func squareMovement():
-	var changes = Vector2(0,0)
-	if Input.is_action_just_pressed('move_right'):
-		changes.x += right
-	if Input.is_action_just_pressed("move_left"):
-		changes.x += left
-	if Input.is_action_just_pressed("move_up"):
-		changes.y += up
-	if Input.is_action_just_pressed("move_down"):
-		changes.y += down
-	$squareSelector.global_position += changes*tileSize
-	return changes
