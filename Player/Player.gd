420d3ba7631c17d4b3cb945383b5b2fe4a8b5b4d
waffletron244 <elijah.crouch244@gmail.com>
2023-10-16 23:23:29 -0600

    TestBoard
    Added a test tileset and set up the player on the board so it looks nice.

--- Player/Player.gd
+++ Player/Player.gd
@@ -18,13 +18,13 @@ var boardRef
 var stage = 0
 
 #Change to board length for proper spacing
-func _ready():
-	if get_parent().name =="Board":
-		if get_parent().tileSize != null:
-			print('hi')
-			tileSize =  get_parent().tileSize
-			boardRef = get_parent()
-			
+func rready():
+	boardRef = get_parent()
+	if get_parent().tileSize != null:
+		tileSize = get_parent().tileSize
+		global_position=(boardRef.BtoW(Vector2(0,0)))
+		print(tileSize)
+		
 	turnFlow()
 
 func _process(delta):
